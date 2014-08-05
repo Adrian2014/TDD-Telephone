045fd618208fbb8877db6bcf44ab365909aadee4
Adrian  & Spencer Zhang <pair+adrian2014.spencer-zhang@devbootcamp.com>
2014-08-05 10:24:00 -0500

    pizza class created

---
+++ pizza/pizza.rb
@@ -0,0 +1,14 @@
+class Pizza
+  attr_accessor :name, :description, :time_baked
+
+
+
+  def self.create(arg)
+    pizza=self.new
+    pizza.name = arg
+    pizza.description = " "
+    pizza.time_baked = 0
+    return pizza
+  end
+
+end
--- pizza/pizza_spec.rb
+++ pizza/pizza_spec.rb
@@ -5,12 +5,13 @@ RSpec.describe Pizza do
   before do
     @pizza = Pizza.create("Pepperoni")
   end
+
   describe "#initialize" do
     it "creates a pizza object" do
       expect(@pizza).to be_a Pizza
     end
     it "should return proper name" do
-      expect(@pizza.name).to be("Pepperoni")
+      expect(@pizza.name).to eq("Pepperoni")
     end
     it "should return proper description" do
       expect(@pizza.description).not_to be_nil
