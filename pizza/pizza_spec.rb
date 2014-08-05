e82dd83b3906f86a9515a4b80fd6816de88ac44c
Ismail Kalimi & Steven Loar <pair+ismk.s-loar@devbootcamp.com>
2014-08-05 09:59:46 -0500

    Finished writing tests for initializing Pizza

---
+++ pizza/pizza_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require_relative 'pizza'
+
+RSpec.describe Pizza do
+  before do
+    @pizza = Pizza.create("Pepperoni")
+  end
+  describe "#initialize" do
+    it "creates a pizza object" do
+      expect(@pizza).to be_a Pizza
+    end
+    it "should return proper name" do
+      expect(@pizza.name).to be("Pepperoni")
+    end
+    it "should return proper description" do
+      expect(@pizza.description).not_to be_nil
+    end
+    it "should return proper time_baked" do
+      expect(@pizza.time_baked).to eq(0)
+    end
+
+  end
+end
